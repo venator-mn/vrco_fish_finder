4929148ad11757c64727be37c3ba21cf4bb58d2b
venator-mn <81487981+venator-mn@users.noreply.github.com>
2023-09-24 18:03:02 -0500

    Add files via upload

---
+++ parse_fish.py
@@ -0,0 +1,20 @@
+import requests
+
+# 46.935866510337604, -94.31460981571192
+url = "http://services.dnr.state.mn.us/api/lakefinder/by_point/v1?lat=46.935866510337604&lon=-94.31460981571192&radius=16093.44"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    for lake in data["results"]:
+        lake_name = lake["name"]
+        fish_species = lake["fishSpecies"]
+        print(f"Lake: {lake_name}")
+        print("Fish Species:")
+        for species in fish_species:
+            print("- " + species)
+        print("~*" * 20)
+else:
+    print(f"Request failed with status code: {response.status_code}")
