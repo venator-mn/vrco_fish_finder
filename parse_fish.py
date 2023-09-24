46839b385a198e4487b5fcffb7e8c5e9ee4dfdf3
venator-mn <81487981+venator-mn@users.noreply.github.com>
2023-09-24 18:04:23 -0500

    Add files via upload

--- parse_fish.py
+++ parse_fish.py
@@ -1,6 +1,6 @@
 import requests
 
-# 46.935866510337604, -94.31460981571192
+# Update the lat and long, leave the radius as-is which is the max allowable
 url = "http://services.dnr.state.mn.us/api/lakefinder/by_point/v1?lat=46.935866510337604&lon=-94.31460981571192&radius=16093.44"
 
 response = requests.get(url)
